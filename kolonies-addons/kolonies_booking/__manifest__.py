0899ea8fd0f2c7c811cd3112fcc53acb48211400
Omar OUHARI <omar.ouhari@gmail.com>
2020-06-21 20:29:28 +0200

    [IMPL]: manage security access

--- kolonies-addons/kolonies_booking/__manifest__.py
+++ kolonies-addons/kolonies_booking/__manifest__.py
@@ -1,29 +1,31 @@
 # -*- coding: utf-8 -*-
 {
-  'name': 'Kolonies Booking',
-  'summary': '',
-  'category': 'Booking',
-  'version': '1.0',
-  'sequence': -95,
-  'author': 'Omar OUHARI',
-  'license': 'Other proprietary',
-  'website': '',
-  'description': '',
-  'depends': [
-    'marketplace_booking_system',
-    'kolonies_base',
-    'calendar',
-  ],
-  'data': [
-    # data
-    'data/ir_sequence.xml',
-    # security
-    'security/ir.model.access.csv',
-    # views
-    'views/res_partner_view.xml',
-    'views/booking_session_view.xml',
-  ],
-  'application': False,
-  'installable': True,
-  'auto_install': False,
+    'name': 'Kolonies Booking',
+    'summary': '',
+    'category': 'Booking',
+    'version': '1.0',
+    'sequence': -95,
+    'author': 'Omar OUHARI',
+    'license': 'Other proprietary',
+    'website': '',
+    'description': '',
+    'depends': [
+        'marketplace_booking_system',
+        'kolonies_base',
+        'calendar',
+    ],
+    'data': [
+        # data
+        'data/ir_sequence.xml',
+        # security
+        'security/res_groups_users.xml',
+        'security/ir_rules.xml',
+        'security/ir.model.access.csv',
+        # views
+        'views/res_partner_view.xml',
+        'views/booking_session_view.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
 }

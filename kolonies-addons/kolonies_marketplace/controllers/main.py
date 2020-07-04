5a50a779da66bdff5e1ed548b97b89090c840287
Omar OUHARI <omar.ouhari@gmail.com>
2020-07-04 15:42:04 +0200

    [FIX]: indentation

--- kolonies-addons/kolonies_marketplace/controllers/main.py
+++ kolonies-addons/kolonies_marketplace/controllers/main.py
@@ -12,42 +12,42 @@ SPR = 4  # Shops/sellers Per Row
 
 class MarketplaceSellerProfile(MarketplaceSellerProfile):
 
-	@http.route([
-		'/sellers/list/',
-		'/sellers/list/page/<int:page>',
-	], type='http', auth="public", website=True)
-	def load_mp_all_seller(self, page=0, search='', localisation='', skills='', ppg=False, **post):
-		print('loc', localisation, 'ski', skills, 'sear', search)
-		if not ppg:
-			ppg = request.env['website'].get_current_website().seller_ppg
-		PPR = request.env['website'].get_current_website().seller_ppr
-		if ppg:
-			try:
-				ppg = int(ppg)
-			except ValueError:
-				ppg = SPG
-			post["ppg"] = ppg
-		else:
-			ppg = SPG
-		domain = self._get_seller_search_domain(search)
-		keep = QueryURL('/sellers/list', search=search)
+    @http.route([
+        '/sellers/list/',
+        '/sellers/list/page/<int:page>',
+    ], type='http', auth="public", website=True)
+    def load_mp_all_seller(self, page=0, search='', localisation='', skills='', ppg=False, **post):
+        print('loc', localisation, 'ski', skills, 'sear', search)
+        if not ppg:
+            ppg = request.env['website'].get_current_website().seller_ppg
+        PPR = request.env['website'].get_current_website().seller_ppr
+        if ppg:
+            try:
+                ppg = int(ppg)
+            except ValueError:
+                ppg = SPG
+            post["ppg"] = ppg
+        else:
+            ppg = SPG
+        domain = self._get_seller_search_domain(search)
+        keep = QueryURL('/sellers/list', search=search)
 
-		url = "/sellers/list"
-		if search:
-			post["search"] = search
+        url = "/sellers/list"
+        if search:
+            post["search"] = search
 
-		seller_obj = request.env['res.partner']
-		seller_count = seller_obj.sudo().search_count(domain)
-		total_active_seller = seller_obj.sudo().search_count(self._get_seller_search_domain(""))
-		pager = request.website.pager(url=url, total=seller_count, page=page, step=ppg, scope=7, url_args=post)
-		seller_objs = seller_obj.sudo().search(domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post))
+        seller_obj = request.env['res.partner']
+        seller_count = seller_obj.sudo().search_count(domain)
+        total_active_seller = seller_obj.sudo().search_count(self._get_seller_search_domain(""))
+        pager = request.website.pager(url=url, total=seller_count, page=page, step=ppg, scope=7, url_args=post)
+        seller_objs = seller_obj.sudo().search(domain, limit=ppg, offset=pager['offset'], order=self._get_search_order(post))
 
-		values = {
-			'search': search,
-			'pager': pager,
-			'seller_objs': seller_objs,
-			'search_count': seller_count,
-			'bins': TableCompute().process(seller_objs, ppg, PPR),
+        values = {
+            'search': search,
+            'pager': pager,
+            'seller_objs': seller_objs,
+            'search_count': seller_count,
+            'bins': TableCompute().process(seller_objs, ppg, PPR),
             'ppg': ppg,
             'ppr': PPR,
             'rows': SPR,

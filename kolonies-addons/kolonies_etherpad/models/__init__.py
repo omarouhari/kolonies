b73d6ff9ba034c220c1d4074923cb91788f94cfb
Omar OUHARI <omar.ouhari@gmail.com>
2020-08-07 00:42:57 +0200

    [IMPL] : add argos website module

--- kolonies-addons/kolonies_etherpad/models/__init__.py
+++ kolonies-addons/kolonies_etherpad/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import ir_attachment
 from . import booking_session
+from . import sale_order_line
--- kolonies-addons/kolonies_etherpad/models/booking_session.py
+++ kolonies-addons/kolonies_etherpad/models/booking_session.py
@@ -2,6 +2,7 @@
 
 import logging
 from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 from odoo.addons.kolonies_base.models.tools import hash_string_hex
 
 _logger = logging.getLogger(__name__)
@@ -17,11 +18,9 @@ class BookingSession(models.Model):
         endpoint = self.env['ir.config_parameter'].sudo().get_param('etherpad_endpoint')
         sequence = self.env['ir.sequence'].next_by_code('etherpad.session.seq')
         if not endpoint:
-            _logger.warning(_('The etherpad endpoint is not defined in system parameters. Please contact your system administrator.'))
-            return False
+            ValidationError(_('The etherpad endpoint is not defined in system parameters. Please contact your system administrator.'))
         if not sequence:
-            _logger.warning(_('The etherpad sequence not found.'))
-            return False
+            ValidationError(_('The etherpad sequence not found.'))
         if not endpoint.endswith('/'):
             endpoint += '/'
         self.iframe_etherpad = endpoint + hash_string_hex(sequence)
